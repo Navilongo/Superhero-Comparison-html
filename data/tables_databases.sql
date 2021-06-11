8bc5b7bfb096a1693f3e726e24874117b9ff1424
Navilongo <navilongo@gmail.com>
2021-06-10 20:08:44 -0700

    updating sql

---
+++ data/tables_databases.sql
@@ -0,0 +1,31 @@
+drop table 
+
+create table hero_teams (
+	id serial Primary Key,
+	superhero varchar, 
+	team varchar,
+	universe varchar,
+	alignment varchar 
+);
+
+select * FROM hero_teams;
+
+create table heroes (
+	id int,
+	universe varchar, 
+	superhero varchar,
+	identity varchar,
+	backstory varchar,
+	powers varchar, 
+	gender varchar, 
+	race varchar, 
+	alignment varchar, 
+	teams varchar
+	intelligence score int,
+	strength score int,
+	speed score int, 
+	
+	
+);
+
+
