8915c59a70e413ccfbf2f72854823b3574b594d3
Ron Fernandez <ronbfernandez@gmail.com>
2021-06-12 01:12:25 -0700

    HTML files updated

--- heroes_app.py
+++ heroes_app.py
@@ -11,6 +11,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/versus")
+def versus():
+    return render_template("versus.html")
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/query")
+def query():
+
+    args = request.args
+
+    print(args)
+
 
 if __name__ =='__main__':
     app.run(debug=True)
